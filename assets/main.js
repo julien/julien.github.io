370d7ccab929bf1e2c885c8f60bda7acc0e3e936
Julien Castelain <julien.castelain@liferay.com>
2020-02-29 17:08:56 +0100

    Formatting

--- assets/main.js
+++ assets/main.js
@@ -1,42 +1,39 @@
-(function () {
-  'use strict';
-  var files = [];
-  var list = document.querySelector('.list');
+(function() {
+	"use strict";
+	var files = [];
+	var list = document.querySelector(".list");
 
-  function listFiles() {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-      var data;
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        data = JSON.parse(xhr.responseText);
-        files = data.files;
+	function listFiles() {
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			var data;
+			if (xhr.readyState === 4 && xhr.status === 200) {
+				data = JSON.parse(xhr.responseText);
+				files = data.files;
 
-        if (files) {
+				if (files) {
+					list.appendChild(fileList(files));
+				}
+			}
+		};
+		xhr.open("GET", "assets/files.json", true);
+		xhr.send(null);
+	}
 
-          list.appendChild(fileList(files));
+	function fileList(files) {
+		var frag = document.createDocumentFragment();
+		for (var i = 0, l = files.length; i < l; i++) {
+			var li = document.createElement("li");
+			var a = document.createElement("a");
+			a.setAttribute("href", files[i]);
+			a.textContent = files[i].replace(/^\w+\//, "");
+			li.appendChild(a);
+			frag.appendChild(li);
+		}
+		return frag;
+	}
 
-        }
-      }
-    };
-    xhr.open('GET', 'assets/files.json', true);
-    xhr.send(null);
-  }
-
-  function fileList(files) {
-    var frag = document.createDocumentFragment();
-    for (var i = 0, l = files.length; i < l; i++) {
-      var li = document.createElement('li');
-      var a = document.createElement('a')
-      a.setAttribute('href', files[i]);
-      a.textContent = files[i].replace(/^\w+\//, '');
-      li.appendChild(a);
-      frag.appendChild(li);
-    }
-    return frag;
-  }
-
-  window.onload = function () {
-    listFiles();
-  };
-
-}());
+	window.onload = function() {
+		listFiles();
+	};
+})();
